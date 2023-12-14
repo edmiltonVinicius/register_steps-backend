7b7b30e9ffbd789f0dbb8c606633146388bf3fd1
Edmilton Pansanato <edmilton.pansanato@goflux.com.br>
2023-12-14 19:38:32 -0300

    feat: return the project - change db number into redis connection

--- domain/redis_connection.go
+++ domain/redis_connection.go
@@ -10,7 +10,7 @@ import (
 var RedisClient *redis.Client
 
 func StartRedis() {
-	_, err := strconv.Atoi(Environment.REDIS_DB)
+	dbNumber, err := strconv.Atoi(Environment.REDIS_DB)
 	if err != nil {
 		log.Println("Error in parsing redis_db from string to int: ", err)
 		return
@@ -19,7 +19,7 @@ func StartRedis() {
 	client := redis.NewClient(&redis.Options{
 		Addr:     Environment.REDIS_URL,
 		Password: Environment.REDIS_PASSWORD,
-		DB:       0,
+		DB:       dbNumber,
 	})
 
 	_, err = client.Ping(Environment.CTX).Result()
